f9c38d6be3ee6991053902bd865bed3057b46464
nivashkin <psix90@yandex.ru>
2018-02-22 13:32:32 +0300

    edit hibernate.cfg

--- src/main/java/ru/ts/bestteam/Main.java
+++ src/main/java/ru/ts/bestteam/Main.java
@@ -1,5 +1,11 @@
 package ru.ts.bestteam;
 
+import org.hibernate.Session;
+
 public class Main {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        System.out.println("run");
+    }
 
 }
